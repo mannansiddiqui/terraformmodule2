59bfe2b4d7ac70f1aa936cee3bb54d1182cd9c07
mannansiddiqui <74168188+mannansiddiqui@users.noreply.github.com>
2023-03-10 14:55:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "Name of this Bucket"
+  default = "testingbucketwithcloudguru12"
+}
+
+variable "env" {
+  description = "Name of environment"
+  default = "dev"
+}
